28e704a9d0c7eb47574c9b61e27a31136454701b
RomanA <126502093+RomanAntoshin@users.noreply.github.com>
2023-04-25 20:03:27 +0300

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -1,41 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace лаб_1__вт_попытка_
 {
     class Point
     {
-        /* double X;
-        double Y;
-        double Z;
-        double T;*/
         public List<double> Vect = new List<double>();
         public double y;
-        /* Point(double x1)
-         {
-             X = x1;
-         }
-         Point(double x1, double x2)
-         {
-             X = x1;
-             Y = x2;
-         }
-         Point(double x1, double x2, double x3)
-         {
-             X = x1;
-             Y = x2;
-             Z = x3;
-         }
-         Point(double x1, double x2, double x3, double x4)
-         {
-             X = x1;
-             Y = x2;
-             Z = x3;
-             T = x4;
-         }*/
         public Point(List<double> x)
         {
             Vect.Clear();
@@ -50,6 +25,20 @@ namespace лаб_1__вт_попытка_
         {
             for (int i = 0; i < n; i++)
                 Vect.Add(0);
+        }
+        public void Print(Point p, TextBox textBox, ListBox listBox)
+        {
+            textBox.Text = "(";
+            foreach (double el in p.Vect)
+                textBox.Text = textBox.Text + el.ToString() + ";" + " ";
+            textBox.Text = textBox.Text + ")";
+            listBox.Items.Add(textBox.Text);
+            textBox.Clear();
+        }
+        public void Print(List<Point> points, TextBox textBox, ListBox listBox)
+        {
+            foreach (Point el in points)
+                el.Print(el, textBox, listBox);
         }
         public static Point operator +(Point p1, Point p2)
         {
